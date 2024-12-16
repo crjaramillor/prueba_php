ba4474ab9e44cf89d2deb5bc5e882cc0fce93567
crjaramillor <cristianjr1292@gmail.com>
2024-12-16 13:13:00 -0500

    Cambio MYSQL-10

--- MYSQL-10.sql
+++ MYSQL-10.sql
@@ -1,14 +1,25 @@
+WITH incidencia_tareas AS (
+    SELECT 
+        p.name AS nombre_propiedad,
+        i.id AS incidencia_id,
+        COUNT(t.id) AS cantidad_no_solucionadas,
+        ROW_NUMBER() OVER (PARTITION BY p.id ORDER BY COUNT(t.id) DESC) AS rn
+    FROM 
+        propiedades p
+    LEFT JOIN 
+        incidencias i ON p.id = i.propiedad_id
+    LEFT JOIN 
+        tareas t ON t.incidencia_id = i.id AND t.estado = 'No Solucionada'
+    GROUP BY 
+        p.id, i.id
+)
 SELECT 
-    p.name AS nombre_propiedad,
-    i.id AS incidencia_id, 
-    COUNT(t.id) AS cantidad_no_solucionadas
+    nombre_propiedad,
+    incidencia_id,
+    cantidad_no_solucionadas
 FROM 
-    propiedades p
-LEFT JOIN 
-    incidencias i ON p.id = i.propiedad_id
-LEFT JOIN 
-    tareas t ON t.incidencia_id = i.id AND t.estado = 'No Solucionada'
-GROUP BY 
-    p.id, i.id
+    incidencia_tareas
+WHERE 
+    rn = 1
 ORDER BY 
     cantidad_no_solucionadas DESC;
